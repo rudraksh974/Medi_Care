883b1c181a2eab62abac190d2c07b5e79f3fd104
rudraksh974 <145356796+rudraksh974@users.noreply.github.com>
2026-02-20 01:02:12 +0530

    Add doctor type function

--- doctors/forms.py
+++ doctors/forms.py
@@ -5,3 +5,10 @@ class DoctorProfileForm(forms.ModelForm):
     class Meta:
         model = Doctor
         fields = ['specialization', 'experience', 'location', 'phone', 'available']
+        widgets = {
+            'specialization': forms.Select(attrs={'class': 'form-control'}),
+            'experience': forms.NumberInput(attrs={'class': 'form-control'}),
+            'location': forms.TextInput(attrs={'class': 'form-control'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control'}),
+            'available': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
