88d2e46a34999127c3dfe0014acbd2b49dadbbb0
rudraksh974 <145356796+rudraksh974@users.noreply.github.com>
2026-02-20 21:41:08 +0530

    Remove default location

--- prediction/urls.py
+++ prediction/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    # path("test-email/", views.test_email), // ONLY FOR TESTING
     path('predict/',views.predict_disease,name='predict_disease'),
 ]
