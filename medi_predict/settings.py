32f5c7e3d27e2de5f4e49cf1a56ac138b48efaaf
rudraksh974 <145356796+rudraksh974@users.noreply.github.com>
2026-07-14 16:18:34 +0530

    update EMAIL_BACKEND to console.EmailBackend

--- medi_predict/settings.py
+++ medi_predict/settings.py
@@ -188,13 +188,14 @@ AUTH_USER_MODEL = 'users.User' # Ye default user ko hta deta hai jo django by de
 
 
 #EMAIL OTP SENDER
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587)) if os.getenv("EMAIL_PORT") else 587
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
+
 
 
 # Media Files Configuration (Doctor Certificates Upload, etc.)
